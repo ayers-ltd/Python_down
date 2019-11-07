dbeb0f51cf8e650664196842e8a1324120a0779a
Mycatchina <1914775620@qq.com>
2019-11-08 03:29:20 +0800

    终极版一键下载全部章节 ".m4d"

---
+++ find_web_m4a_v2.py
@@ -0,0 +1,27 @@
+import requests
+import re
+"""import pprint is json.print"""
+
+"""webclient.headers"""
+heads = {}
+heads['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0'
+
+"""song_name,song_id down ok"""
+def download_madia(song_id, song_name):
+    media_url = 'https://www.ximalaya.com/revision/play/v1/audio?id=' + song_id + '&ptype=1'
+    response = requests.get(media_url, headers=heads)
+    data = response.json()
+    mp3_url = data['data']['src']
+    response_copy = requests.get(mp3_url)
+    with open(song_name + '.m4a', 'wb') as f:
+        f.write(response_copy.content)
+    print(song_name,' download is ok')
+
+"""list down """
+for range_num in range(1, 9):
+    response_html = requests.get('https://www.ximalaya.com/xiangsheng/6355825/p' + str(range_num) + '/',  headers=heads)
+    html_ml = re.findall('<div class="text _c2"><a title="(.*?)" href="/xiangsheng/6355825/(.*?)">', response_html.text)
+    for filename in html_ml:
+        print(filename)
+    """start '#' is down list """
+        #download_madia(filename[1],filename[0])
